bcf6cf50b7d6409d9891b3f324ece392d9c41b58
Emdadul_Islam <emdadulislam439@gmail.com>
2022-08-01 14:07:14 +0600

    read local html file without BeautifulSoup

---
+++ BeautifulSoup/FreeCodeCamp/main.py
@@ -0,0 +1,5 @@
+from bs4 import BeautifulSoup
+
+with open('/home/emdad439/Developing/Django Journey/Learning Web Scraping/BeautifulSoup/FreeCodeCamp/example_website.html', 'r') as html_file:
+    page_content = html_file.read()
+    print(page_content)
