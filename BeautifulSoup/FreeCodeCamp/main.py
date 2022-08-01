d594fc33c2a939569abb2d20d002284c87f1d593
Emdadul_Islam <emdadulislam439@gmail.com>
2022-08-01 14:09:31 +0600

    printed the prettified file with beautifulSoup

--- BeautifulSoup/FreeCodeCamp/main.py
+++ BeautifulSoup/FreeCodeCamp/main.py
@@ -2,4 +2,7 @@ from bs4 import BeautifulSoup
 
 with open('/home/emdad439/Developing/Django Journey/Learning Web Scraping/BeautifulSoup/FreeCodeCamp/example_website.html', 'r') as html_file:
     page_content = html_file.read()
-    print(page_content)
+    # print(page_content)
+
+    soup = BeautifulSoup(page_content, 'lxml')
+    print(soup.prettify())
