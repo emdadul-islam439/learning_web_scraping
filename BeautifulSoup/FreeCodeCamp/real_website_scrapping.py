6d5663b66aaadb1acc6e00c4b5b08b83d5cbcc0b
Emdadul_Islam <emdadulislam439@gmail.com>
2022-08-02 02:41:45 +0600

    extracted 'exparience' and 'job_location' by reading documentation

--- BeautifulSoup/FreeCodeCamp/real_website_scrapping.py
+++ BeautifulSoup/FreeCodeCamp/real_website_scrapping.py
@@ -16,15 +16,19 @@ job_card_list = soup.find_all("li", class_ = "clearfix job-bx wht-shd-bx")
 
 for job in job_card_list:
     published = job.find("span", class_= "sim-posted").span.text.replace('\n', '')
-
     if 'few' in published:
         company_name = job.find("h3", class_ = "joblist-comp-name").text.replace('\n', '')
         key_skills = job.find("span", class_="srp-skills").text.replace(' ', '').replace('\n', '')
+
+        exparience_and_location_sectionn = job.find("ul", class_="top-jd-dtl clearfix")
+        experience_needed = exparience_and_location_sectionn.li.contents[1]
+        job_location = exparience_and_location_sectionn.find_all("li")[1].span.text
         print(company_name)
         print(key_skills)
         print(published)
+        
+        print(experience_needed)
+        print(job_location)
         print()
 
 # job_description = ""
-# experience_needed = ""
-# job_location = ""
