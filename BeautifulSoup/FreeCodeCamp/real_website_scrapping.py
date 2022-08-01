88bd0a52823a6e1720fcfac3f3cb85952c65e2e6
Emdadul_Islam <emdadulislam439@gmail.com>
2022-08-01 19:49:51 +0600

    got all the job information

--- BeautifulSoup/FreeCodeCamp/real_website_scrapping.py
+++ BeautifulSoup/FreeCodeCamp/real_website_scrapping.py
@@ -12,16 +12,18 @@ soup = BeautifulSoup(content, 'lxml')
 
 total_result_count = soup.find('span', id = "totolResultCountsId").text
 
-job_card_list = soup.find("li", class_ = "clearfix job-bx wht-shd-bx")
+job_card_list = soup.find_all("li", class_ = "clearfix job-bx wht-shd-bx")
 
+for job in job_card_list:
+    company_name = job.find("h3", class_ = "joblist-comp-name").text.replace('\n', '')
+    key_skills = job.find("span", class_="srp-skills").text.replace(' ', '').replace('\n', '')
+    published = job.find("span", class_= "sim-posted").span.text.replace(' ', '').replace('\n', '')
 
-company_name = job_card_list.find("h3", class_ = "joblist-comp-name").text
-key_skills = job_card_list.find("span", class_="srp-skills").text.replace(' ', '')
-published = job_card_list.find("span", class_= "sim-posted").span.text
+    print(company_name)
+    print(key_skills)
+    print(published)
+    print()
 
-print(company_name)
-print(key_skills)
-print(published)
-job_description = ""
-experience_needed = ""
-job_location = ""
+# job_description = ""
+# experience_needed = ""
+# job_location = ""
