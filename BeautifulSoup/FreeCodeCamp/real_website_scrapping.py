8ba99b9f3c768fad7d25ff4a367d8507c15556d9
Emdadul_Islam <emdadulislam439@gmail.com>
2022-08-01 18:00:17 +0600

    got all job card data(s)[not an organized list]

--- BeautifulSoup/FreeCodeCamp/real_website_scrapping.py
+++ BeautifulSoup/FreeCodeCamp/real_website_scrapping.py
@@ -1,4 +1,3 @@
-from urllib.request import Request
 from bs4 import BeautifulSoup
 import requests
 
@@ -6,7 +5,11 @@ import requests
 html_file = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=')
 # print(html_file)  #output: <Response [200]>
 content = html_file.content
-print(content) #not a 'prettier' HTML content
+# print(content)  #not a 'prettier' HTML content
 
 soup = BeautifulSoup(content, 'lxml')
-print(soup.prettify())
+# print(soup.prettify())  #prettier output 
+
+total_result_count = soup.find('span', id = "totolResultCountsId")
+job_card_list = soup.find_all("li", class_ = "clearfix job-bx wht-shd-bx")
+print(job_card_list[0].text)
