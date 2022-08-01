5d97a7bc742d67a28f78d846e5d149ed5f5df21e
Emdadul_Islam <emdadulislam439@gmail.com>
2022-08-01 19:52:57 +0600

    practiced 'adding' custom filtering [printed only the job info which have published 'few' days ago]

--- BeautifulSoup/FreeCodeCamp/real_website_scrapping.py
+++ BeautifulSoup/FreeCodeCamp/real_website_scrapping.py
@@ -15,14 +15,15 @@ total_result_count = soup.find('span', id = "totolResultCountsId").text
 job_card_list = soup.find_all("li", class_ = "clearfix job-bx wht-shd-bx")
 
 for job in job_card_list:
-    company_name = job.find("h3", class_ = "joblist-comp-name").text.replace('\n', '')
-    key_skills = job.find("span", class_="srp-skills").text.replace(' ', '').replace('\n', '')
-    published = job.find("span", class_= "sim-posted").span.text.replace(' ', '').replace('\n', '')
+    published = job.find("span", class_= "sim-posted").span.text.replace('\n', '')
 
-    print(company_name)
-    print(key_skills)
-    print(published)
-    print()
+    if 'few' in published:
+        company_name = job.find("h3", class_ = "joblist-comp-name").text.replace('\n', '')
+        key_skills = job.find("span", class_="srp-skills").text.replace(' ', '').replace('\n', '')
+        print(company_name)
+        print(key_skills)
+        print(published)
+        print()
 
 # job_description = ""
 # experience_needed = ""
