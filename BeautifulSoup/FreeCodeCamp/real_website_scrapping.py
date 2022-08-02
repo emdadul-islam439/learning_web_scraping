e91ae0d49434931dd35f41f4268e8aa93b2806c6
Emdadul_Islam <emdadulislam439@gmail.com>
2022-08-02 17:27:40 +0600

    requested with 10 minutes interval

--- BeautifulSoup/FreeCodeCamp/real_website_scrapping.py
+++ BeautifulSoup/FreeCodeCamp/real_website_scrapping.py
@@ -1,3 +1,4 @@
+import time
 from bs4 import BeautifulSoup
 import requests
 
@@ -5,28 +6,35 @@ print("Put a skill you are unfamiliar with")
 unfamiliar_skill = input('>')
 print(f"Filtering out {unfamiliar_skill}")
 
-## requesting to the 'timesjob' website
-html_file = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=')
-# print(html_file)  #output: <Response [200]>
-content = html_file.content
-# print(content)  #not a 'prettier' HTML content
 
-soup = BeautifulSoup(content, 'lxml')
-# print(soup.prettify())  #prettier output 
+def find_job():
+    html_file = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=')
+    content = html_file.content
 
-total_result_count = soup.find('span', id = "totolResultCountsId").text
+    soup = BeautifulSoup(content, 'lxml')
+    # print(soup.prettify())  #prettier output 
 
-job_card_list = soup.find_all("li", class_ = "clearfix job-bx wht-shd-bx")
+    total_result_count = soup.find('span', id = "totolResultCountsId").text
 
-for job in job_card_list:
-    published = job.find("span", class_= "sim-posted").span.text.replace('\n', '')
-    if 'few' in published:
-        company_name = job.find("h3", class_ = "joblist-comp-name").text.replace('\n', '').strip()
-        key_skills = job.find("span", class_="srp-skills").text.replace(' ', '').replace('\n', '')
-        more_info = job.find("ul", class_ = "list-job-dtl clearfix").li.a['href']
+    job_card_list = soup.find_all("li", class_ = "clearfix job-bx wht-shd-bx")
 
-        if unfamiliar_skill not in key_skills:
-            print(f"Company Name: {company_name}")
-            print(f"Required Skills: {key_skills}")
-            print(f"More Info: {more_info}")
-            print()
+    for job in job_card_list:
+        published = job.find("span", class_= "sim-posted").span.text.replace('\n', '')
+        if 'few' in published:
+            company_name = job.find("h3", class_ = "joblist-comp-name").text.replace('\n', '').strip()
+            key_skills = job.find("span", class_="srp-skills").text.replace(' ', '').replace('\n', '')
+            more_info = job.find("ul", class_ = "list-job-dtl clearfix").li.a['href']
+
+            if unfamiliar_skill not in key_skills:
+                print(f"Company Name: {company_name}")
+                print(f"Required Skills: {key_skills}")
+                print(f"More Info: {more_info}")
+                print()
+
+
+if __name__== '__main__':
+    while True:
+        find_job()
+        time_wait = 10
+        print(f"Waiting {time_wait} minutes...")
+        time.sleep(time_wait * 60)
