8c0ccaf349d5d7a29b88a16511bcc12799285406
Emdadul_Islam <emdadulislam439@gmail.com>
2022-08-02 18:21:59 +0600

    dynamically written job posts to different txt files

--- BeautifulSoup/FreeCodeCamp/real_website_scrapping.py
+++ BeautifulSoup/FreeCodeCamp/real_website_scrapping.py
@@ -18,18 +18,19 @@ def find_job():
 
     job_card_list = soup.find_all("li", class_ = "clearfix job-bx wht-shd-bx")
 
-    for job in job_card_list:
+    for index, job in enumerate(job_card_list):
         published = job.find("span", class_= "sim-posted").span.text.replace('\n', '')
         if 'few' in published:
             company_name = job.find("h3", class_ = "joblist-comp-name").text.replace('\n', '').strip()
-            key_skills = job.find("span", class_="srp-skills").text.replace(' ', '').replace('\n', '')
+            key_skills = job.find("span", class_="srp-skills").text.replace(' ', '').replace('\n', '').strip()
             more_info = job.find("ul", class_ = "list-job-dtl clearfix").li.a['href']
 
             if unfamiliar_skill not in key_skills:
-                print(f"Company Name: {company_name}")
-                print(f"Required Skills: {key_skills}")
-                print(f"More Info: {more_info}")
-                print()
+                with open(f"/home/emdad439/Developing/Django Journey/Learning Web Scraping/BeautifulSoup/FreeCodeCamp/written_documents/job_post_no.{index}.txt", 'w') as file_writer:
+                    file_writer.write(f"Company Name: {company_name}\n")
+                    file_writer.write(f"Required Skills: {key_skills}\n")
+                    file_writer.write(f"More Info: {more_info}\n")
+                print(f"Successfully saved the file no. {index}")
 
 
 if __name__== '__main__':
