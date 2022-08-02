3ca9a0b72c18dfa149828cf323fd8e67cf60ded0
Emdadul_Islam <emdadulislam439@gmail.com>
2022-08-02 17:20:16 +0600

    practiced a filetering technique (filtered the unfamiliar skills)

--- BeautifulSoup/FreeCodeCamp/real_website_scrapping.py
+++ BeautifulSoup/FreeCodeCamp/real_website_scrapping.py
@@ -1,6 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
 
+print("Put a skill you are unfamiliar with")
+unfamiliar_skill = input('>')
+print(f"Filtering out {unfamiliar_skill}")
+
 ## requesting to the 'timesjob' website
 html_file = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=')
 # print(html_file)  #output: <Response [200]>
@@ -20,7 +24,9 @@ for job in job_card_list:
         company_name = job.find("h3", class_ = "joblist-comp-name").text.replace('\n', '').strip()
         key_skills = job.find("span", class_="srp-skills").text.replace(' ', '').replace('\n', '')
         more_info = job.find("ul", class_ = "list-job-dtl clearfix").li.a['href']
-        print(f"Company Name: {company_name}")
-        print(f"Required Skills: {key_skills}")
-        print(f"More Info: {more_info}")
-        print()
+
+        if unfamiliar_skill not in key_skills:
+            print(f"Company Name: {company_name}")
+            print(f"Required Skills: {key_skills}")
+            print(f"More Info: {more_info}")
+            print()
