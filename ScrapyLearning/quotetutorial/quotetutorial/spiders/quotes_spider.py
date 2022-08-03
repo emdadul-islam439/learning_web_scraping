450c03ff212fcc472341fb425e8c305c001e55ed
Emdadul_Islam <emdadulislam439@gmail.com>
2022-08-03 17:04:31 +0600

    fixed the issue: 'importError: not foudn 'scrapy' by installing 'scrapy' in parent 'quotetutorial' folder

---
+++ ScrapyLearning/quotetutorial/quotetutorial/spiders/quotes_spider.py
@@ -0,0 +1,14 @@
+import imp
+
+
+import scrapy
+
+class QuoteSpider(scrapy.Spider):
+    name = "quotes"
+    start_urls = [
+        "https://quotes.toscrape.com/"
+    ]
+
+    def parse(self, response):
+        title = response.css('title').extract()
+        yield {'titletext' : title}
