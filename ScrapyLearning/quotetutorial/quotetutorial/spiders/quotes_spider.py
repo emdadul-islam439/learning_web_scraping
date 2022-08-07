76424882a884c83edb4e967004d8422b4c57e46b
Emdadul_Islam <emdadulislam439@gmail.com>
2022-08-07 21:22:29 +0600

    BUG REPORT: when doing all the tasks within parse() function, everything becomes okay

--- ScrapyLearning/quotetutorial/quotetutorial/spiders/quotes_spider.py
+++ ScrapyLearning/quotetutorial/quotetutorial/spiders/quotes_spider.py
@@ -1,15 +1,17 @@
-from gc import callbacks
 import scrapy
+from scrapy.http import FormRequest
 from ..items import QuotetutorialItem
 
 class QuoteSpider(scrapy.Spider):
     name = "quotes"
-    next_page_number = 2
+    next_page_number = - 1
     start_urls = [
-        "https://quotes.toscrape.com/page/1/"
+        # "http://quotes.toscrape.com/login"
+        "http://quotes.toscrape.com/"
     ]
 
     def parse(self, response):
+        # self.start_scraping(response = response)
         item = QuotetutorialItem()
         all_quote_card = response.css("div.quote")
 
@@ -23,8 +25,53 @@ class QuoteSpider(scrapy.Spider):
             item['tags'] = tags
         
             yield item
+        
+        # token = response.css("input::attr(value)").get()
+        # print(f"CSRF TOKEN = {token}............................")
+        # FormRequest.from_response(response, formdata={
+        #     'csrf_token' : token,
+        #     'username' : "emdad",
+        #     'password' : "hello"
+        # }, callback = self.start_scraping)
 
-            next_page = f"https://quotes.toscrape.com/page/{QuoteSpider.next_page_number}/"
-            if QuoteSpider.next_page_number < 11:
-                QuoteSpider.next_page_number += 1
-                yield response.follow(next_page, callback = self.parse)
+    
+    # def start_scraping(self, response):
+    #     item = QuotetutorialItem()
+    #     all_quote_card = response.css("div.quote")
+
+    #     for quote_info in all_quote_card:
+    #         title = quote_info.css("span.text::text").get()
+    #         author = quote_info.css("small.author::text").get()
+    #         tags = quote_info.css("a.tag::text").getall()
+
+    #         item['title'] = title
+    #         item['author'] = author
+    #         item['tags'] = tags
+        
+    #         yield item
+
+
+    # def start_scraping(self, response):
+    #     if QuoteSpider.next_page_number == -1:
+    #         QuoteSpider.next_page_number = 1
+    #         next_page = f"https://quotes.toscrape.com/page/{QuoteSpider.next_page_number}/"
+    #         yield response.follow(next_page, callback = self.start_scraping)
+
+    #     item = QuotetutorialItem()
+    #     all_quote_card = response.css("div.quote")
+
+    #     for quote_info in all_quote_card:
+    #         title = quote_info.css("span.text::text").get()
+    #         author = quote_info.css("small.author::text").get()
+    #         tags = quote_info.css("a.tag::text").getall()
+
+    #         item['title'] = title
+    #         item['author'] = author
+    #         item['tags'] = tags
+        
+    #         yield item
+
+    #         next_page = f"https://quotes.toscrape.com/page/{QuoteSpider.next_page_number}/"
+    #         if QuoteSpider.next_page_number < 11:
+    #             QuoteSpider.next_page_number += 1
+    #             yield response.follow(next_page, callback = self.start_scraping)
