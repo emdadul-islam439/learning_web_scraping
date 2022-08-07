175f1611c64e42395e93060fc64069e2de74b230
Emdadul_Islam <emdadulislam439@gmail.com>
2022-08-08 01:00:57 +0600

    Before FIXING: did not use 'yield' before FormRequest.from_response(...), could not get expected output

--- ScrapyLearning/quotetutorial/quotetutorial/spiders/quotes_spider.py
+++ ScrapyLearning/quotetutorial/quotetutorial/spiders/quotes_spider.py
@@ -6,34 +6,19 @@ class QuoteSpider(scrapy.Spider):
     name = "quotes"
     next_page_number = - 1
     start_urls = [
-        # "http://quotes.toscrape.com/login"
-        "http://quotes.toscrape.com/"
+        "http://quotes.toscrape.com/login"
+        # "http://quotes.toscrape.com/"
     ]
 
     def parse(self, response):
-        print("in the PARSE() function...................................")
-        return self.start_scraping(response)
-        # item = QuotetutorialItem()
-        # all_quote_card = response.css("div.quote")
-
-        # for quote_info in all_quote_card:
-        #     title = quote_info.css("span.text::text").get()
-        #     author = quote_info.css("small.author::text").get()
-        #     tags = quote_info.css("a.tag::text").getall()
-
-        #     item['title'] = title
-        #     item['author'] = author
-        #     item['tags'] = tags
-        
-        #     yield item
-        
-        # token = response.css("input::attr(value)").get()
-        # print(f"CSRF TOKEN = {token}............................")
-        # FormRequest.from_response(response, formdata={
-        #     'csrf_token' : token,
-        #     'username' : "emdad",
-        #     'password' : "hello"
-        # }, callback = self.start_scraping)
+        print("in the PARSE() function................||||||||||||||||||||||||||...................")
+        token = response.css("input::attr(value)").get()
+        print(f"CSRF TOKEN = {token}...............|||||||||||||||||||||||||||.............")
+        FormRequest.from_response(response, formdata={
+            "csrf_token" : token,
+            "username" : "emdad",
+            "password" : "hello"
+        }, callback = self.start_scraping)
 
     
     def start_scraping(self, response):
