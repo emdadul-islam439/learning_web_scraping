87f039d6ca978d0b69d3b1d8a2db407506040fb9
Emdadul_Islam <emdadulislam439@gmail.com>
2022-08-07 14:21:17 +0600

    got all the 'quote', 'author', and 'tag' info one by one

--- ScrapyLearning/quotetutorial/quotetutorial/spiders/quotes_spider.py
+++ ScrapyLearning/quotetutorial/quotetutorial/spiders/quotes_spider.py
@@ -7,13 +7,15 @@ class QuoteSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        quote_card = response.css("div.quote")[0]
-        title = quote_card.css("span.text::text").get()
-        author = quote_card.css("small.author::text").get()
-        tags = quote_card.css("a.tag::text").getall()
-    
-        yield { 
-            'title' : title,
-            'author' : author,
-            'tags' : tags, 
-         }   
+        all_quote_card = response.css("div.quote")
+
+        for quote_info in all_quote_card:
+            title = quote_info.css("span.text::text").get()
+            author = quote_info.css("small.author::text").get()
+            tags = quote_info.css("a.tag::text").getall()
+        
+            yield { 
+                'title' : title,
+                'author' : author,
+                'tags' : tags, 
+            }   
