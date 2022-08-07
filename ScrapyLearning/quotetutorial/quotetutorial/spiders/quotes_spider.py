a8c4867d23f567f678e84d42c70fd02f56060ed7
Emdadul_Islam <emdadulislam439@gmail.com>
2022-08-08 01:06:15 +0600

    scraped all the 10 pages after Logging In

--- ScrapyLearning/quotetutorial/quotetutorial/spiders/quotes_spider.py
+++ ScrapyLearning/quotetutorial/quotetutorial/spiders/quotes_spider.py
@@ -4,7 +4,7 @@ from ..items import QuotetutorialItem
 
 class QuoteSpider(scrapy.Spider):
     name = "quotes"
-    next_page_number = - 1
+    next_page_number = 2
     start_urls = [
         "http://quotes.toscrape.com/login"
         # "http://quotes.toscrape.com/"
@@ -20,9 +20,8 @@ class QuoteSpider(scrapy.Spider):
             "password" : "hello"
         }, callback = self.start_scraping)
 
-    
+        
     def start_scraping(self, response):
-        print("in the start_scraping() function...................................")
         item = QuotetutorialItem()
         all_quote_card = response.css("div.quote")
 
@@ -37,28 +36,7 @@ class QuoteSpider(scrapy.Spider):
         
             yield item
 
-
-    # def start_scraping(self, response):
-    #     if QuoteSpider.next_page_number == -1:
-    #         QuoteSpider.next_page_number = 1
-    #         next_page = f"https://quotes.toscrape.com/page/{QuoteSpider.next_page_number}/"
-    #         yield response.follow(next_page, callback = self.start_scraping)
-
-    #     item = QuotetutorialItem()
-    #     all_quote_card = response.css("div.quote")
-
-    #     for quote_info in all_quote_card:
-    #         title = quote_info.css("span.text::text").get()
-    #         author = quote_info.css("small.author::text").get()
-    #         tags = quote_info.css("a.tag::text").getall()
-
-    #         item['title'] = title
-    #         item['author'] = author
-    #         item['tags'] = tags
-        
-    #         yield item
-
-    #         next_page = f"https://quotes.toscrape.com/page/{QuoteSpider.next_page_number}/"
-    #         if QuoteSpider.next_page_number < 11:
-    #             QuoteSpider.next_page_number += 1
-    #             yield response.follow(next_page, callback = self.start_scraping)
+            next_page = f"https://quotes.toscrape.com/page/{QuoteSpider.next_page_number}/"
+            if QuoteSpider.next_page_number < 11:
+                QuoteSpider.next_page_number += 1
+                yield response.follow(next_page, callback = self.start_scraping)
